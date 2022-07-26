b05abce771cbb8f1da481dacf0972e0a72bbd491
Zaeem629 <zaeemuzzafar@gmail.com>
2022-07-27 04:16:44 +0500

    changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,18 @@ import img2 from '../images/login-abs-img-2.png'
 import img3 from '../images/index-banner-abs-3.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebookF , faSkype ,faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { GoogleLogin } from 'react-google-login'
 function Login() {
+
+  const GoogleSuccessfulresponse  = async (response) => {
+    console.log("success",response)
+
+  }
+
+  const GoogleFailurefulresponse  = async (response) => {
+    console.log("failure",response)
+  }
+
   return (
     <>
         <section className="login-sec inner-pg-banner about-banner">
@@ -22,6 +33,12 @@ function Login() {
               </div>
               <a href="#!" className="forget-btn">Forgot Password</a>
               <a href="#!" className="login-btn">Login</a>
+                <GoogleLogin
+                    clientId='819534639726-95ggtcfrqo0indihojndn1js3iqjr73b.apps.googleusercontent.com'
+                    buttonText=""
+                    onSuccess={(e , response) => GoogleSuccessfulresponse(e , response)}
+                    onFailure={(e , response) => GoogleFailurefulresponse(e , response)}
+                />
               <p className="bottom-tagline">Dont have an acount? <a href="#!">Signup</a></p>	
             </form>
             <ul className="socil-list list-unstyled">
